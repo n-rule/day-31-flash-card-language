19fbc6d62f73c39f70c531abb3b00839d6ffcf94
n-rule <rule@ukr.net>
2021-02-10 15:55:24 +0200

    1

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from tkinter import *
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP -----------------------------
+
+window = Tk()
+window.title('Flashy Cards')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front = PhotoImage(file='./images/card_front.png')
+card_back = PhotoImage(file='./images/card_back.png')
+
+canvas.create_image(400, 260, image=card_front)
+canvas.grid(column='0', row='0', columnspan='2')
+
+image_right = PhotoImage(file='./images/right.png')
+button_right = Button(image=image_right)
+button_right.grid(column='1', row='1')
+
+image_wrong = PhotoImage(file='./images/wrong.png')
+button_wrong = Button(image=image_wrong)
+button_wrong.grid(column='0', row='1')
+
+window.mainloop()
+
